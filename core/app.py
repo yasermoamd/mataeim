6758f307f75d816a41cbb03eab5aff87999f1ea4
yasermoamd <yasermoamd@hotmail.com>
2024-03-31 11:08:54 +0100

    updated

--- core/app.py
+++ core/app.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from .extentions import db
+from .router.main import main_bp
+
 
 def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = ""
-    app.config["SECRET_KEY"] = ""
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasteway.db"
+    app.config["SECRET_KEY"] = "CXGWWwO/VfH14hH2ID2NsSNQR0K6WOmlhuKZ+4tdEHk="
 
+    # register routes blueprints
+    app.register_blueprint(main_bp, url_prefix="/")
     db.init_app(app)
 
+    app.debug = True
     return app
