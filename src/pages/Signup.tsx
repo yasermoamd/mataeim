1ba53102bb69968540813ca4ebb7cd2cf03f00ca
yasermoamd <yasermoamd@hotmail.com>
2024-04-16 20:51:43 +0100

    signup page

---
+++ src/pages/Signup.tsx
@@ -0,0 +1,12 @@
+import SignupForm from '../components/SignupForm'
+import AuthLayout from '../layouts/AuthLayout'
+
+function Signup() {
+  return (
+    <AuthLayout>
+        <SignupForm />
+    </AuthLayout>
+  )
+}
+
+export default Signup
