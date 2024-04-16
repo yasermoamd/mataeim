60f540f705c70ec71f8254133516f64d3f022a99
yasermoamd <yasermoamd@hotmail.com>
2024-04-16 20:44:31 +0100

    simple routing

---
+++ src/main.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+import Login from './pages/Login.tsx'
+import Signup from './pages/Signup.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/signup',
+    element: <Signup />
+  }
+])
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
