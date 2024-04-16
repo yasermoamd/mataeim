9a87e00398627a9ed3bf89ddb5fe3b4bd1030e83
yasermoamd <yasermoamd@hotmail.com>
2024-04-16 02:36:03 +0100

    header component

---
+++ client/src/components/HeaderComponent.tsx
@@ -0,0 +1,15 @@
+const HeaderComponent = () => {
+  return (
+    <header className='flex justify-around items-center'>
+    <div>
+      <h1>My Logo</h1>
+    </div>
+    <div className='flex gap-4'>
+      <div>Feed</div>
+      <div>Friends</div>
+    </div>
+  </header>
+  )
+}
+
+export default HeaderComponent
