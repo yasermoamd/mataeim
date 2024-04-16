9b222193b45843f13b4054ae10c74210202f5c1a
be-great <begreat5333@gmail.com>
2024-04-17 02:10:57 +0300

    add some docker configuration

--- backend/app.py
+++ backend/app.py
@@ -3,11 +3,19 @@ from extensions import db
 from routes import auth_bp
 
 
+
+
 def create_app():
-    
+    DATABASE_URL = "sqlite:///mydatabase.db"
     app = Flask(__name__)
     app.config.from_prefixed_env()
-
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.register_blueprint(auth_bp, url_prefix='/api')
     db.init_app(app)
     return app
+
+quotes_app = create_app()
+# Run the Flask app if this script is executed directly
+if __name__ == "__main__":
+    quotes_app.run()
