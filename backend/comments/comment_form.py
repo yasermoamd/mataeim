75ffb26305946d5ad55b62a880d7f5555f444661
be-great <begreat5333@gmail.com>
2024-04-18 18:39:16 +0300

    update comment form

--- backend/comments/comment_form.py
+++ backend/comments/comment_form.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm as Form
-from wtforms import StringField, TextAreaField
+from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class CommentForm(Form):
     text = TextAreaField(u'content', validators=[DataRequired()])
+    submit = SubmitField('Submit')
--- backend/comments/comment_routes.py
+++ backend/comments/comment_routes.py
@@ -3,18 +3,33 @@ from util.extensions import db
 from posts.post_model import Post
 from .comment_model import Comment
 from .comment_form import CommentForm
-
+from users.user_model import User
+from flask_jwt_extended import jwt_required, get_jwt_identity
 comment_routes = Blueprint('comments', __name__)
 
 @comment_routes.route('/', methods=['GET'])
+@jwt_required()
 def get_comments():
     comments = Comment.query.all()
     return jsonify([comment.__dict__ for comment in comments])
 
 @comment_routes.route('/create', methods=['POST'])
+@jwt_required()
 def create_comment():
+    # check user id from the jwt header
+    current_user_id = get_jwt_identity()
+    user = User.get_user_by_id(current_user_id)
+    if not user:
+        return jsonify({"Error": "User not found"}), 404
     form = CommentForm()
     if form.validate_on_submit():
+        content = form.content.data
+        comment = Comment(content = content, user_id = user.id)
+        comment.create_comment()
+        return jsonify({"message": "Comment created successfully", "comment": comment.__dict__}), 201
+    else:
+        return jsonify({"error": "Validation error", "errors": form.errors}), 400
+
         
     # if request.method == 'POST':
     #     data = request.get_json()
