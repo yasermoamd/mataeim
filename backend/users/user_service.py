08455da101f70731510d9e8974cfa5d8690049ee
yasermoamd <yasermoamd@hotmail.com>
2024-04-24 02:30:30 +0100

    update: get user by email, and authenticate by email

--- backend/users/user_service.py
+++ backend/users/user_service.py
@@ -1,12 +1,12 @@
 """
 This module contains the service layer for the User model.
 """
-def get_user_by_username(cls, fullname: str):
-    return cls.query.filter_by(fullname=fullname).first()
+def get_user_by_username(cls, email: str):
+    return cls.query.filter_by(email=email).first()
 
-def authenticate(fullname, password):
+def authenticate(email, password):
     from .user_model import User
-    user = User.query.filter_by(fullname=fullname).first()
+    user = User.query.filter_by(email=email).first()
     if not user:
         return None
     # Do the passwords match
