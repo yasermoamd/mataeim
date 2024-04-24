1570eb6eaf4a9810bab3bc390f60e585f0a6076f
yasermoamd <yasermoamd@hotmail.com>
2024-04-24 13:20:09 +0100

    updated: router removed

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,28 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
   RouterProvider
 } from 'react-router-dom'
-import Login from './pages/Login.tsx'
-import Signup from './pages/Signup.tsx'
+import { router } from './router.tsx'
 import { Provider } from 'react-redux'
-import  store from './app/store/store.ts'
-import AuthLayout from './layouts/AuthLayout.tsx'
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route>
-      <Route path="/" element={<App />} />,
-      <Route path="/login" element={<Login />} />,
-      <Route path="/signup" element={<Signup />} />,
-    </Route>
-  )
-)
+import store from './app/store/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
