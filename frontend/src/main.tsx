0af9e4c569047c124968099e4bd2a201f493f27c
yasermoamd <yasermoamd@hotmail.com>
2024-04-24 02:33:25 +0100

    update: redux and simple routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,27 +4,30 @@ import App from './App.tsx'
 import './index.css'
 import {
   createBrowserRouter,
+  createRoutesFromElements,
+  Route,
   RouterProvider
 } from 'react-router-dom'
 import Login from './pages/Login.tsx'
 import Signup from './pages/Signup.tsx'
+import { Provider } from 'react-redux'
+import  store from './app/store/store.ts'
+import AuthLayout from './layouts/AuthLayout.tsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />
-  },
-  {
-    path: '/login',
-    element: <Login />
-  },
-  {
-    path: '/signup',
-    element: <Signup />
-  }
-])
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<App />} />,
+      <Route path="/login" element={<Login />} />,
+      <Route path="/signup" element={<Signup />} />,
+    </Route>
+  )
+)
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+     <Provider store={store}>
+       <RouterProvider router={router} />
+     </Provider>
   </React.StrictMode>,
 )
